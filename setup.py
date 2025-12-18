7f4008ed031e9a8afe75b889620898e33f7d45a9
lusciouslukey <git-shit@chezbennett.com>
2025-12-18 10:14:30 +0000

    pkorpine nr7101 referenced

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'ping3==2.9.1',
         'influxdb-client==1.19.0',
-        'nr5103 @ git+https://github.com/pkorpine/nr5103.git@v1.3.0',
+        'nr7101 @ git+https://github.com/pkorpine/nr7101.git@v1.3.0',
         'rx==3.2.0'
     ],
     entry_points={
